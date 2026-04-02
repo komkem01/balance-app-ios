c5d7ef05347303cf838e1a0ea2b8a0339e98ea73
komkem <komkem.contact@gmail.com>
2026-04-02 20:39:51 +0700

    Initial Commit

---
+++ balance-app-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  balance-app-ios
+//
+//  Created by komkem khamket on 2/4/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
